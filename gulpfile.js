2cf00d1b3c522c196e787e04732952024a8cf228
Sigmund <sigmund.rolfsjord@gmail.com>
2015-10-22 22:36:37 +0200

    Gjorde endringer etter nye forespørsler fra Markus. Prøver å fikse ikke alignet bilder i oldtids safari.

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var debug = require('gulp-debug');
+
+
+gulp.task('default', function () {
+    return gulp.src('assets/css/*.scss')
+        .pipe(debug())
+        .pipe(sass())
+        .pipe(prefix("last 3 version", "> 1%", "ie 8", "ie 7"))
+        .pipe(gulp.dest('assets/css/'));
+});
